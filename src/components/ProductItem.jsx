55cad5e9ba18f909a7841eeb7774c984db29469b
Siddhartha Dey Chetia <excutioner01@gmail.com>
2026-02-02 15:44:41 +0530

    added link to ProductDetail page in ProductItem component

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,21 +1,25 @@
+import { Link } from "react-router-dom";
+
 const ProductItem = ({ product, onAddToCart }) => {
-    return (
-      <div className="product-item">
+  return (
+    <div className="product-item">
+      <Link to={`/products/${product.id}`}>
         <img
           src={product.thumbnail}
           alt={product.title}
           className="product-image"
           loading="lazy"
         />
-  
         <h3>{product.title}</h3>
-        <p>₹{product.price}</p>
-  
-        <button onClick={() => onAddToCart(product)}>
-          Add to Cart
-        </button>
-      </div>
-    );
-  };
-  
-  export default ProductItem;
+      </Link>
+
+      <p>₹{product.price}</p>
+
+      <button onClick={() => onAddToCart(product)}>
+        Add to Cart
+      </button>
+    </div>
+  );
+};
+
+export default ProductItem;
