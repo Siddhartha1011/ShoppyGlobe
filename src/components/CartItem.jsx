59083a25ebef74abbd4a9662408e5ae07526a669
Siddhartha Dey Chetia <excutioner01@gmail.com>
2026-02-02 21:46:57 +0530

    add CartItem component to display individual cart items with quantity controls

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,30 @@
+import { useDispatch } from "react-redux";
+import {
+  removeFromCart,
+  increaseQuantity,
+  decreaseQuantity,
+} from "../redux/cartSlice";
+
+const CartItem = ({ item }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="cart-item">
+      <img src={item.thumbnail} alt={item.title} width="80" />
+      <div className="cart-item-info">
+        <h4>{item.title}</h4>
+        <p>Price: ₹{item.price}</p>
+        <div className="quantity-controls">
+          <button onClick={() => dispatch(decreaseQuantity(item.id))}>-</button>
+          <span>{item.quantity}</span>
+          <button onClick={() => dispatch(increaseQuantity(item.id))}>+</button>
+        </div>
+        <button onClick={() => dispatch(removeFromCart(item.id))}>
+          Remove
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
