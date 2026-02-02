3a8cddea8be17c8bd0d4a2a6592d7be981f19e12
Siddhartha Dey Chetia <excutioner01@gmail.com>
2026-02-02 16:01:15 +0530

    Add routing for product list and product detail pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import ProductList from "./components/ProductList";
+import ProductDetail from "./components/ProductDetail";
 
 function App() {
   return (
     <>
       <Header />
-      <main>
-        <ProductList />
-      </main>
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+      </Routes>
     </>
   );
 }
